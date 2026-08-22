62178b5edbab620f8e60dedaea9ec88967584f66
qutubuddinkhan07 <qutubuddinkhan8261@gmail.com>
2026-08-22 13:06:59 +0530

    completed 20/08/26 task

---
+++ React Class/my_app/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-600 h-full w-full">
+      <h1 className="bg-amber-500 font-extrabold text-[300px]">Bootstrap</h1>
+    </div>
+  );
+};
+
+export default App;

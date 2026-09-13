dfceac8678ae6da470112dd807eeeb37c60c8b7a
qutubuddinkhan07 <qutubuddinkhan8261@gmail.com>
2026-09-13 09:33:51 +0530

    learned nested routing, universal path (*), modern way of routing

--- React Class/my_app/src/concepts/routing/components/Routing.jsx
+++ React Class/my_app/src/concepts/routing/components/Routing.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Nav from "./Nav";
@@ -7,27 +13,110 @@ import Product from "./Product";
 import Order from "./Order";
 import Cart from "./Cart";
 import Wishlist from "./Wishlist";
+import PageNotFound from "./PageNotFound";
+import Layout from "./Layout";
+
+// const Routing = () => {
+//   return (
+//! Before using Layout
+// <div>
+//   <BrowserRouter>
+//     <Nav />  {/* Removing this from here and using it in <Layout/> component /> */}
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       {/* <Route path="/product" element={<Product />} /> */}
+//       <Route path="/about" element={<About />} />
+//       <Route path="/order-summary" element={<Order />} />
+
+//       {/* //! Nested Routing Concept */}
+//       <Route path="/product" element={<Product />}>
+//         <Route index element={<Cart />} />
+//         <Route path="cart" element={<Cart />} />
+//         <Route path="wishlist" element={<Wishlist />} />
+//       </Route>
+
+//       {/* //! Universal route to handle the not showing anything on the page */}
+//       <Route path="*" element={<PageNotFound />} />
+//     </Routes>
+//   </BrowserRouter>
+// </div>
+
+//! after using Layout
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Home />} />
+//             {/* <Route path="/product" element={<Product />} /> */}
+//             <Route path="/about" element={<About />} />
+//             <Route path="/order-summary" element={<Order />} />
+
+//             {/* //! Nested Routing Concept */}
+//             <Route path="/product" element={<Product />}>
+//               <Route index element={<Cart />} />
+//               <Route path="cart" element={<Cart />} />
+//               <Route path="wishlist" element={<Wishlist />} />
+//             </Route>
+//           </Route>
+
+//           {/* //! Universal route to handle the not showing anything on the page */}
+//           <Route path="*" element={<PageNotFound />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
+
+// export default Routing;
+
+//! Modern way of routing
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+        children: [
+          //! Uncaught Error: Absolute route path "/cart" nested under path "/product" is not valid. An absolute child route path must start with the combined path of all its parent routes
+          //! **WE CAN DIRECT USE NAME IN NESTED PATH WITHOUT '/'
+          { index: true, element: <Cart /> },
+          {
+            path: "cart",
+            element: <Cart />,
+          },
+          {
+            path: "wishlist",
+            element: <Wishlist />,
+          },
+        ],
+      },
+      {
+        path: "/order-summary",
+        element: <Order />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 const Routing = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/product" element={<Product />} /> */}
-          <Route path="/about" element={<About />} />
-          <Route path="/order-summary" element={<Order />} />
-
-          {/* //! Nested Routing Concept */}
-          <Route path="/product" element={<Product />}>
-            <Route index element={<Cart />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="wishlist" element={<Wishlist />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <RouterProvider router={routes}></RouterProvider>
+    </>
   );
 };
 
